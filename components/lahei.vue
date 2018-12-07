783f0669554a685d181fdd06ac3b818b3b40b827
nlh1996 <1217286494@qq.com>
2018-12-07 16:47:49 +0800

    拉黑界面1.1

---
+++ components/lahei.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <table bgcolor='black' spellcheck="1px" class="table1">
+      <tr bgcolor='white'>
+        <td class="name">用户名</td>
+        <td class="time">拉黑日期</td>
+        <td class="action">操作</td>
+      </tr>
+      <tr v-for="(index) in 5" bgcolor='white' class="text" :key="index">
+        <td class="name"><div class="text"></div></td>
+        <td class="time"><div class="text"></div></td>
+        <td class="action"><div class="text text-button" @click="yichu">移出</div></td>
+      </tr>
+    </table>
+
+    <van-pagination 
+      v-model="currentPage" 
+      :page-count="12"
+      mode="simple" 
+    />
+
+    <van-search
+      v-model="value"
+      placeholder="请输入用户名"
+      show-action 
+      @search="onSearch"
+    >
+      <div slot="action" @click="onSearch">搜索</div>
+    </van-search>
+
+    <table bgcolor='black' spellcheck="1px" class="table2">
+      <tr bgcolor='white'>
+        <td class="name">用户名</td>
+        <td>等级</td>
+        <td>积分</td>
+        <td>创建时间</td>
+        <td>操作</td>
+      </tr>
+      <tr bgcolor='white'>
+        <td class="name"><div class="text">窃@格瓦拉</div></td>
+        <td><div class="text"></div></td>
+        <td><div class="text"></div></td>
+        <td><div class="text"></div></td>
+        <td><div class="text text-button" @click="jiaru">加入</div></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        currentPage: 1,
+        value: ''
+      }
+    },
+    methods: {
+      yichu() {
+        console.log(111)
+      },
+      jiaru() {
+        console.log(222)
+      },
+      onSearch() {
+        this.value = ''
+      },
+    }
+  }
+</script>
+
+<style>
+.table1{
+  width: 6rem;
+  height: 3rem;
+  margin: 0 auto;
+  margin-top: 0.2rem;
+}
+.table2{
+  width: 6.4rem;
+  height: 1rem;
+  margin: 0 auto;
+  margin-top: 0.2rem;
+}
+.name{
+  width: 1.5rem;
+  text-align: left;
+}
+.time{
+  width: 3rem;
+
+}
+.action{
+  width: 1rem;
+}
+.text{
+  font-size: 0.26rem
+}
+.text-button{
+  color: red;
+}
+
+.van-search{
+  margin-top: 0.4rem;
+}
+
+.van-cell__left-icon{
+  line-height: 0.5rem;
+}
+</style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -3,7 +3,28 @@
   <div class="view view-admin">
     <van-tabs v-model="active" sticky color="orange" type="card" @click="onClick">
       <van-tab title="贴子管理"><div class="box-admin">内容 1</div></van-tab>
-      <van-tab title="用户管理"><div class="box-admin">内容 2</div></van-tab>
+      <van-tab title="用户管理">
+        <div class="box-admin">
+          <van-tabs v-model="active2">
+            <van-tab class="van-tab">
+              <div slot="title">
+                <van-icon name="records"/>黑名单
+              </div>
+              <div class="box-border"> 
+                <lahei></lahei>
+              </div> 
+            </van-tab>
+
+            <van-tab class="van-tab">
+              <div slot="title">
+                <van-icon name="chat"/>禁言
+              </div>
+              <div class="box-border">
+              </div> 
+            </van-tab>
+          </van-tabs>
+        </div>
+      </van-tab>
       <van-tab title="数据统计"><div class="box-admin">
         <van-cell-group>
           <van-cell title="今日访问量:" :value="value1+'次'" /> 
@@ -19,15 +40,20 @@
 
 <script>
 import axios from '../../http'
+import lahei from '@/components/lahei'
   export default {
     data() {
       return {
         active: 0,
+        active2: 0,
         value1: '',
         value2: '',
         value3: '',
         value4: '',
       }
+    },
+    components: {
+      lahei: lahei
     },
     methods: {
       onClick(index) {
@@ -56,15 +82,39 @@ import axios from '../../http'
   background-color: white
 }
 .box-admin{
-  width: 6rem;
-  height: 7rem;
+  width: 6.9rem;
+  height: 10rem;
   background-color: white;
   margin: 0 auto;
-  padding: 0.5rem;
+  padding: 0.3rem;
   margin-top: 0.2rem;
 }
 
 .van-cell {
-  line-height: 30px;
+  line-height: 0.5rem;
+}
+
+.van-tab{
+  height: 0.56rem;
+  line-height: 0.5rem;
+}
+
+.van-tabs--card .van-tabs__wrap{
+  height: 0.6rem;
+}
+
+.van-tabs__nav--card{
+  height: 0.6rem;
+}
+
+.van-tabs__nav--card .van-tab{
+  line-height: 0.58rem;
+}
+
+.box-border{
+  margin-top: 0.2rem;
+  width: 100%;
+  height: 7.5rem;
+  border: 1px solid lightgray;
 }
 </style>
