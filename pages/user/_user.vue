5d963a6ba78cb7cfa1923833c9e7bf892895055d
nlh1996 <1217286494@qq.com>
2018-12-14 16:09:13 +0800

    初步完善个人中心

--- pages/user/_user.vue
+++ pages/user/_user.vue
@@ -2,10 +2,20 @@
   <div class="view">
     <header-view :name="title"></header-view>
     <div class="user-data">
-      <div class="headImg">               
-        <img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2564997198,4187947589&fm=58" style="width:0.8rem"/>
+      <div class="headImg d1">               
+        <img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2564997198,4187947589&fm=58" style="width:1.2rem"/>
       </div>
-      <span>{{form.user}}</span>
+      <div class="d2">
+        <div class="row1">
+          <div class="item name">{{form.uName}}</div>
+          <div class="item jifen">当前积分:{{form.integral}}</div>
+        </div>
+        <div class="row2">
+          <div>获得的阅读<div class="num">0</div></div>
+          <div class="left-border">获得的回复<div class="num">{{form.replyNum}}</div></div>
+          <div class="left-border">获得的赞<div class="num">{{form.support}}</div></div>
+        </div>
+      </div>
     </div>
 
     <van-cell-group>
@@ -36,6 +46,7 @@ import headerView from '~/components/header'
     },
     computed: {
       form: function() {
+        console.log(this.$store.state.login.userdata)
         return this.$store.state.login.userdata || ''
       }
     },
@@ -48,7 +59,7 @@ import headerView from '~/components/header'
 <style scoped>
 .user-data{
   display: flex;
-  height: 1rem;
+  height: 1.5rem;
   line-height: 1rem;
 }
 
@@ -60,5 +71,43 @@ import headerView from '~/components/header'
 .content{
   text-align: center;
   min-height: 40vh;
+}
+
+.d1,.d2{
+  display: inline-block;
+  height: 1.5rem;
+}
+.item{
+  display: inline-block;
+  margin-left: 0.2rem;
+}
+.row1{
+  height: 0.4rem;
+  line-height: 0.6rem;
+}
+.row2{
+  display: flex;
+  justify-content: space-between;
+  height: 1.5rem;
+}
+.row2 div{
+  margin-left: 0.2rem;
+  margin-right: 0.4rem;
+  color: gray;
+  height: 0.4rem;
+}
+
+.name{
+  font-size: 0.5rem;
+  font-weight: bold;
+}
+.jifen{
+  margin-left: 0.5rem;
+}
+.row2 .num{
+  height: 0.3rem;
+  font-size: 0.3rem;
+  line-height: 0rem;
+  color: black;
 }
 </style>
